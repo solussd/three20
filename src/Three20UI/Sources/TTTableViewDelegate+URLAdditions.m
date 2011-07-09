ed03d5b94543cd8db53356b073f09a7c91fd889e
Joseph Smith <joe@uwcreations.com>
2011-07-09 16:13:50 -0500

    added TTTableViewDelegate+URLAdditions

---
+++ src/Three20UI/Sources/TTTableViewDelegate+URLAdditions.m
@@ -0,0 +1,23 @@
+//
+//  TTTableViewDelegate+URLAdditions.m
+//  Three20UI
+//
+//  Created by Joseph Smith on 7/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTTableViewDelegate+URLAdditions.h"
+
+@implementation TTTableViewDelegate_URLAdditions
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
